1733abce7d1269f79732a3d280e0e2100118e59a
Diego Selzlein <diegoselzlein@gmail.com>
2023-12-07 14:43:16 -0300

    Improvements and fixes.

--- translate_subs.rb
+++ translate_subs.rb
@@ -44,27 +44,37 @@ ARGV.each do |file|
 
   i = tracks.find_index { |t| t['Track type'] == 'subtitles' }
 
+  unless i
+    puts("#{file} - no subtitle track")
+    next
+  end
+
   en_srt = "#{path.basename('.*')}.en.srt"
   en_ass = "#{path.basename('.*')}.en.ass"
   pt_srt = "#{path.basename('.*')}.srt"
 
-
-  if tracks.dig(i, 'Codec ID')&.include?('ASS')
-    `mkvextract tracks #{Shellwords.escape(file)} #{i}:#{Shellwords.escape(en_ass)}`
-    asstosrt(en_ass)
-    `rm #{Shellwords.escape(en_ass)}`
-  else
-    `mkvextract tracks #{Shellwords.escape(file)} #{i}:#{Shellwords.escape(en_srt)}`
+  unless File.exist?(en_srt)
+    if tracks.dig(i, 'Codec ID')&.include?('ASS')
+      `mkvextract tracks #{Shellwords.escape(file)} #{i}:#{Shellwords.escape(en_ass)}`
+      asstosrt(en_ass)
+      `rm #{Shellwords.escape(en_ass)}`
+    else
+      `mkvextract tracks #{Shellwords.escape(file)} #{i}:#{Shellwords.escape(en_srt)}`
+    end
   end
 
   # Join split lines for better translation
   srt = SRT::File.parse(File.new(en_srt))
+  srt.to_s # make sure we didn't fail parsing
+
   total = srt.lines.size
-  srt.lines.each_with_index do |l, idx|
-    l.text = [`trans -no-warn -no-autocorrect -brief en:pt-BR #{Shellwords.escape(l.text.join(' '))}`.to_s.strip] if l.text.join(' ').size > 2
-    printf("\r#{file} - %0.1f%%", (idx + 1) * 100.0 / total)
+  progress = 0
+  srt.lines.each_slice(1) do |slice|
+    slice.map { |l| Thread.new { l.text = [`trans -no-warn -no-autocorrect -brief en:pt-BR #{Shellwords.escape(l.text.join(' '))}`.to_s.strip] if l.text.join(' ').scan(/\w/).size > 1 } }.each(&:join)
+    progress += slice.size
+    printf("\r#{file} - %0.1f%%", progress * 100.0 / total)
   end
-  printf("\r#{file} - %0.1f%%", 100.0)
+  printf("\r#{file} - %0.1f%%\n", 100.0)
   IO.write(pt_srt, srt.to_s)
 
   `rm #{Shellwords.escape(en_srt)}`
